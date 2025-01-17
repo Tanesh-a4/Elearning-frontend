fe8564f007eacd14081cfaaef2cfa25308d2d7c5
Tanesh <tanesh753@gmail.com>
2025-01-17 17:08:43 +0530

    loading animation

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./Loading.css";
-import { ScatterBoxLoader } from "react-awesome-loaders";
 
 const Loading = () => {
   return (
-    <ScatterBoxLoader
-    primaryColor={"#6366F1"}
-    background={theme.colors["background"]}
-  />
+    <div className="loading-page">
+      <div className="loader"></div>
+    </div>
   );
 };
 
